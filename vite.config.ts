c935cac847f542319be174884e47ebfc68b95a3c
2311183647-23 <2311183647@qq.com>
2026-01-15 02:17:33 +0800

    Remove all existing files and components for the Moon Phase band official website, including main application logic, styles, assets, and related components.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     return {
+      base: '/band-final/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -21,7 +22,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
         }
       }
     };
